b543ae75f2834064ecbe460fac1413356b9b35a3
Denis Rubtsov <trambalda@gmail.com>
2022-01-19 00:54:59 +0300

    Initial Commit

---
+++ Test_SwiftUI_SlotMachine/Test_SwiftUI_SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_SwiftUI_SlotMachineApp.swift
+//  Test_SwiftUI_SlotMachine
+//
+//  Created by Денис Рубцов on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_SwiftUI_SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
